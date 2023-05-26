4f40cfb3d4afe14e4278b08fed6534045f08d5bf
FakiriYassine <yassineyassine0012@gmail.com>
2023-05-26 15:08:42 +0200

    Phase 19 : gestion des images

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -3,16 +3,23 @@
 namespace App\Entity;
 
 use App\Repository\VisiteRepository;
+use DateTime;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=VisiteRepository::class)
+ * @Vich\Uploadable
  */
-class Visite
-{
+class Visite {
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -60,105 +67,109 @@ class Visite
      */
     private $environnements;
 
-    public function __construct()
-    {
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="visites", fileNameProperty="imageName")
+     * @Assert\Image(mimeTypes="image/jpeg")
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string|null
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    public function __construct() {
         $this->environnements = new ArrayCollection();
     }
 
-    public function getId(): ?int
-    {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getVille(): ?string
-    {
+    public function getVille(): ?string {
         return $this->ville;
     }
 
-    public function setVille(string $ville): self
-    {
+    public function setVille(string $ville): self {
         $this->ville = $ville;
 
         return $this;
     }
 
-    public function getPays(): ?string
-    {
+    public function getPays(): ?string {
         return $this->pays;
     }
 
-    public function setPays(string $pays): self
-    {
+    public function setPays(string $pays): self {
         $this->pays = $pays;
 
         return $this;
     }
 
-    public function getDatecreation(): ?DateTimeInterface
-    {
+    public function getDatecreation(): ?DateTimeInterface {
         return $this->datecreation;
     }
 
-    public function setDatecreation(?DateTimeInterface $datecreation): self
-    {
+    public function setDatecreation(?DateTimeInterface $datecreation): self {
         $this->datecreation = $datecreation;
 
         return $this;
     }
 
-    public function getNote(): ?int
-    {
+    public function getNote(): ?int {
         return $this->note;
     }
 
-    public function setNote(?int $note): self
-    {
+    public function setNote(?int $note): self {
         $this->note = $note;
 
         return $this;
     }
 
-    public function getAvis(): ?string
-    {
+    public function getAvis(): ?string {
         return $this->avis;
     }
 
-    public function setAvis(?string $avis): self
-    {
+    public function setAvis(?string $avis): self {
         $this->avis = $avis;
 
         return $this;
     }
 
-    public function getTempmin(): ?int
-    {
+    public function getTempmin(): ?int {
         return $this->tempmin;
     }
 
-    public function setTempmin(?int $tempmin): self
-    {
+    public function setTempmin(?int $tempmin): self {
         $this->tempmin = $tempmin;
 
         return $this;
     }
 
-    public function getTempmax(): ?int
-    {
+    public function getTempmax(): ?int {
         return $this->tempmax;
     }
 
-    public function setTempmax(?int $tempmax): self
-    {
+    public function setTempmax(?int $tempmax): self {
         $this->tempmax = $tempmax;
 
         return $this;
     }
-    
-    public function getDatecreationString() : string
-    {
-        if($this->datecreation == null){
+
+    public function getDatecreationString(): string {
+        if ($this->datecreation == null) {
             return "";
-        }else{
+        } else {
             return $this->datecreation->format('d/m/Y');
         }
     }
@@ -166,13 +177,11 @@ class Visite
     /**
      * @return Collection<int, Environnement>
      */
-    public function getEnvironnements(): Collection
-    {
+    public function getEnvironnements(): Collection {
         return $this->environnements;
     }
 
-    public function addEnvironnement(Environnement $environnement): self
-    {
+    public function addEnvironnement(Environnement $environnement): self {
         if (!$this->environnements->contains($environnement)) {
             $this->environnements[] = $environnement;
         }
@@ -180,10 +189,63 @@ class Visite
         return $this;
     }
 
-    public function removeEnvironnement(Environnement $environnement): self
-    {
+    public function removeEnvironnement(Environnement $environnement): self {
         $this->environnements->removeElement($environnement);
 
         return $this;
     }
+
+    function getImageFile(): ?File {
+        return $this->imageFile;
+    }
+
+    function getImageName(): ?string {
+        return $this->imageName;
+    }
+
+    function setImageFile(?File $imageFile) {
+        $this->imageFile = $imageFile;
+        if ($this->imageFile instanceof UploadedFile) {
+            $this->updated_at = new DateTime('now');
+        }
+        return $this;
+    }
+
+    function setImageName(?string $imageName) {
+        $this->imageName = $imageName;
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * @Assert\Callback
+     * @param ExecutionContextInterface $context
+     */
+    public function validate(ExecutionContextInterface $context) {
+        $image = $this->getImageFile();
+        if ($image != null && $image != "") {
+            $tailleImage = @getimagesize($image);
+            if (!($tailleImage == false)) {
+                if ($tailleImage[0] > 1300 || $tailleImage[1] > 1300) {
+                    $context->buildViolation("Cette image est trop grande (1300x1300 max)")
+                            ->atPath('imageFile')
+                            ->addViolation();
+                }
+            } else {
+                $context->buildViolation("Ce n'est pas une image")
+                        ->atPath('imageFile')
+                        ->addViolation();
+            }
+        }
+    }
+
 }
